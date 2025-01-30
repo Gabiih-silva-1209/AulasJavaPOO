17b55d717426dacf7e25e2c5095aa351b0a2968a
Gabrieli da Silva <gabih.12161@gmail.com>
2025-01-30 11:11:50 -0300

    Introdução Poo - 30/01/2025

---
+++ src/main/java/com/senai/aula01_introducao_poo/exemplos/pessoa/Main.java
@@ -0,0 +1,9 @@
+package com.senai.aula01_introducao_poo.exemplos.pessoa;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    Pessoa pessoa1 = new Pessoa();
+    LSOutput
+}
---
+++ src/main/java/com/senai/aula01_introducao_poo/exemplos/pessoa/Pessoa.java
@@ -0,0 +1,16 @@
+package com.senai.aula01_introducao_poo.exemplos.pessoa;
+
+public class Pessoa {
+    String name;
+    int idade;
+    float altura;
+    String endereço;
+
+    public void falar(String frase) {
+        System.out.println(frase);
+
+        public void comer (String alimento){
+            System.out.println("A pessoa comeu" + alimento);
+        }
+    }
+}
