5f42d1f941a14954288930a9992b342e39672a3b
Gabrieli da Silva <gabih.12161@gmail.com>
2025-02-20 10:24:09 -0300

    1º exercício e 2º exercício de Java - 20/20/2025

---
+++ src/main/java/exercício03/ControleDeEestoque.java
@@ -0,0 +1,126 @@
+package exercício03;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ControleDeEestoque {
+    public static void main(String[] args) {
+        ArrayList<String> produtos = new ArrayList<>();
+        ArrayList<Integer> quantidades = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("\n Controle de Estoque");
+            System.out.println("1- Cadastrar produto");
+            System.out.println("2- Listar produtos");
+            System.out.println("3- Atualizar quantidade");
+            System.out.println("4- Remover produto");
+            System.out.println("5-Buscar produto");
+            System.out.println("6- Sair");
+            System.out.println("Opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o nome do produto: ");
+                    String nome = scanner.nextLine();
+                    System.out.println("Digite a quantidade do produto: ");
+                    int quantidade = scanner.nextInt();
+                    scanner.nextLine();
+                    produtos.add(nome);
+                    quantidades.add(quantidade);
+                    System.out.println("Produto cadastrado com sucesso!!!");
+                    break;
+
+                case 2:
+                    if (produtos.isEmpty()) {
+                        System.out.println("Não há produtos cadastrados!!!");
+                    } else {
+                        System.out.println("\n Lista de produtos: ");
+                        for (int i = 0; i < produtos.size(); i++) {
+                            System.out.println((i + 1) + "-" + produtos.get(i) + "(Quantidade: " + quantidades.get(i) + ")");
+                        }
+                    }
+                    break;
+                case 3:
+                    if (produtos.isEmpty()) {
+                        System.out.println("Não há produtos cadastrados!!!");
+                    } else {
+                        System.out.println("Digite o nome do produto para atualizar a quantidade: ");
+                        String nomeBusca = scanner.nextLine();
+                        for (int i = 0; i < produtos.size(); i++) {
+                            if (produtos.get(i).equals(nomeBusca)) {
+                                System.out.println("Digite a nova quantidade: ");
+                                int novaQuantidade = scanner.nextInt();
+                                scanner.nextLine();
+                                quantidades.set(i, novaQuantidade);
+                                System.out.println("Quantidade atualizada com sucesso!!!");
+                                break;
+                            }
+                        }
+                        System.out.println("Produto não encontrado!!!");
+                    }
+                    break;
+                case 4:
+                    if (produtos.isEmpty()) {
+                        System.out.println("Não há produtos cadastrados!!!");
+                    } else {
+                        System.out.println("Digite o nome do produto para remover: ");
+                        String nomeBusca = scanner.nextLine();
+                        for (int i = 0; i < produtos.size(); i++) {
+                            if (produtos.get(i).equals(nomeBusca)) {
+                                produtos.remove(i);
+                                System.out.println("Produto removido com sucesso!!");
+                                break;
+                            }
+                        }
+                        System.out.println("Produto não encontrado!!!");
+                    }
+                    break;
+                case 5:
+                    if (produtos.isEmpty()) {
+                        System.out.println("Não há produtos cadastrados.");
+                    } else {
+                        System.out.println("Digite o nome do produto para buscar: ");
+                        String nomeBusca = scanner.nextLine();
+                        for (int i = 0; i < produtos.size(); i++) {
+                            if (produtos.get(i).equals(nomeBusca)) {
+                                System.out.println("Produto encontrado: " + produtos.get(i) + "(Quantidade: " + quantidades.get(i) + ")");
+                                break;
+                            }
+                        }
+                        System.out.println("Produto encontrado!!!");
+                    }
+                    break;
+                case 6:
+                    System.out.println("Saindo do programa!!!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!!");
+            }
+        } while (opcao != 6);
+        scanner.close();
+    }
+}
+class Produto{
+    private String nome;
+    private int quantidade;
+
+    public Produto(String nome, int quantidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+    public String getNome (){
+        return nome;
+    }
+    public int getQuantidade(){
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
