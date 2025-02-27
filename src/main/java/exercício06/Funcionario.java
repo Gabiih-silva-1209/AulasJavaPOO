38eb721762c1adb4470b247749462b838f0ea1f9
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2025-02-27 18:26:34 -0300

    Exercícios - 27/02/2025

---
+++ src/main/java/exercício06/Funcionario.java
@@ -0,0 +1,58 @@
+package exercício06;
+
+public class Funcionario {
+    private String nome;
+    private double salario;
+    private String cargo;
+
+    public Funcionario(String nome, double salario, String cargo) {
+        setNome(nome);
+        setSalario(salario);
+        setCargo(cargo);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null && !nome.isEmpty()) {
+            this.nome = nome;
+        } else {
+            System.out.println("Nome inválido. O nome não pode ser nulo ou vazio.");
+        }
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        if (salario >= 1320) {
+            this.salario = salario;
+        } else {
+            System.out.println("Salário inválido. O salário deve ser maior que R$ 1320,00");
+        }
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        if (cargo.equals("Gerente")) {
+            this.cargo = cargo;
+        } else {
+            System.out.println("Cargo inválido. O cargo deve Gerente, Analista ou Programador.");
+        }
+    }
+
+    public void calcularAumento(double percentual) {
+        if (percentual > 0) {
+            salario += salario * (percentual / 100);
+            System.out.println("Aumento de " + percentual + "% aplicado com sucesso!!");
+        } else {
+            System.out.println("Percentual de aumento inválido. O percentual dever ser positivo.");
+        }
+    }
+}
---
+++ src/main/java/exercício06/GerenciamentoFuncionarios.java
@@ -0,0 +1,94 @@
+package exercício06;
+
+import exercício05.ContaBancaria;
+
+import javax.crypto.spec.PSource;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GerenciamentoFuncionarios {
+    public static void main(String[] args) {
+        ArrayList<Funcionario> funcionarios = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        String menu = """
+                _____________________________________________________________________
+                |   Escolha uma Opção:                                              |
+                |       1- Cadastrar Funcionário                                    |
+                |       2- Listar Funcionários                                      |
+                |       3- Aumento Salário                                          |
+                |       5- Sair                                                     |
+                |___________________________________________________________________|
+                """;
+        int opcao;
+
+        do {
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    cadastrarFuncionario(funcionarios, scanner);
+                    break;
+
+                case 2:
+                    listarFuncionarios(funcionarios);
+                    break;
+
+                case 3:
+                    aumentarSalario(funcionarios, scanner);
+                    break;
+
+                case 4:
+                    System.out.println("Sair");
+
+                default:
+                    System.out.println("Opção inválida!!");
+            }
+        } while (opcao != 4);
+
+        scanner.close();
+    }
+
+    public static void cadastrarFuncionario(ArrayList<Funcionario> funcionarios, Scanner scanner) {
+        System.out.println("Digite o nome o funcionário: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o salário do funcionário: ");
+        double salario = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Digite o cargo do funcionário (Gerente, Analista ou Programador): ");
+        String cargo = scanner.nextLine();
+
+        funcionarios.add(new Funcionario(nome, salario, cargo));
+        System.out.println("Funcionário cadastrado com sucesso!!!");
+
+    }
+
+    public static void listarFuncionarios(ArrayList<Funcionario> funcionarios) {
+        if (funcionarios.isEmpty()) {
+            System.out.println("Nenhum funcionário cadastrado!");
+            for (Funcionario funcionario : funcionarios) {
+                System.out.println("Nome: " + funcionario.getNome() + ", Salário: R$ " + funcionario.getSalario() + ", Cargo: " + funcionario.getCargo());
+            }
+        }
+    }
+
+    public static void aumentarSalario(ArrayList<Funcionario> funcionarios, Scanner scanner) {
+        System.out.println("Digite o nome do funcionário: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite  o percentual de aumento: ");
+        double percentual = scanner.nextDouble();
+        scanner.nextLine();
+
+        for (Funcionario funcionario : funcionarios){
+            if (funcionario.getNome().equals(nome)){
+                funcionario.calcularAumento(percentual);
+                return;
+            }
+        }
+        System.out.println("Funcionário não encontrado.");
+    }
+}
