ea28815d598c84649bd01eac47c024ab6418e727
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2025-03-13 11:08:50 -0300

    Conteúdo sobre Herença - 13/03/2025

--- src/main/java/com/senai/aula04_Heranca/Exemplos/gerenciamento_de_banco_bancarias/ContaPoupanca.java
+++ src/main/java/com/senai/aula04_Heranca/Exemplos/gerenciamento_de_banco_bancarias/ContaPoupanca.java
@@ -1,4 +1,13 @@
 package com.senai.aula04_Heranca.Exemplos.gerenciamento_de_banco_bancarias;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends exercício05Encapsulamento02.ContaBancaria {
+    private double taxaDeRendimento;
+
+    public ContaPoupanca(String titular, double saldoInicial, double taxaDeRendimento) {
+        super(titular, saldoInicial);
+        this.taxaDeRendimento = taxaDeRendimento;
+    }
+    public void aplicarRendimento(){
+        saldo+= (saldo*taxaDeRendimento)/100;
+    }
 }
